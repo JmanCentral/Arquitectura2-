b08fe2daabf50debf5df5d40b69b7be2ae864998
JmanCentral <Jmancipet@ucentral.edu.co>
2024-11-05 19:44:32 -0500

    Programa

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.myapplication;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+
+import java.security.Permission;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,10 +32,57 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        tvlocalizacion = findViewById(R.id.tv_localizacion);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        getCurrentLocation();
     }
+
+    private void getCurrentLocation() {
+        if (PackageManager.PERMISSION_GRANTED != ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+                ActivityCompat.requestPermissions(this, new String[]
+                                {Manifest.permission.ACCESS_FINE_LOCATION,
+                                        Manifest.permission.ACCESS_COARSE_LOCATION},
+                        REQUEST_CODE_LOCATION_PERMISSION);
+
+                return;
+
+            }
+            fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this, location ->
+
+            {
+
+                if (location != null) {
+                    tvlocalizacion.setText("latitud" + location.getLatitude() + "\n" + "longitud" + location.getLongitude());
+                } else {
+
+                    tvlocalizacion.setText("No se pudo obtener la localización");
+                }
+            });
+
+
+        }
+
+    }
+
+    public void onRequestPermissionsResult(int RequestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
+
+        super.onRequestPermissionsResult(RequestCode, permissions,grantResults);
+
+        if (RequestCode == REQUEST_CODE_LOCATION_PERMISSION && grantResults.length > 0)
+        {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            {
+                getCurrentLocation();
+
+            }else {
+                tvlocalizacion.setText("Permiso de ubicación denegado");
+
+            }
+
+        }
+    }
+
 }
+
